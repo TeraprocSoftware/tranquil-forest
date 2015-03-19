fc9ef44f61e1cffb62d292813a855af679fe4c8b
wongda <danielyhwong@gmail.com>
2015-03-19 11:43:37 -0400

    Sign Out

--- public/app/account/tpNavBarLoginCtrl.js
+++ public/app/account/tpNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('tpNavBarLoginCtrl', function($scope, $http, tpIdentity, tpNotifier, tpAuth) {
+angular.module('app').controller('tpNavBarLoginCtrl', function($scope, $http, tpIdentity, tpNotifier, tpAuth, $location) {
     $scope.identity = tpIdentity;
     $scope.signin = function(username, password) {
         tpAuth.authenticateUser(username, password).then(function(success) {
@@ -9,7 +9,12 @@ angular.module('app').controller('tpNavBarLoginCtrl', function($scope, $http, tp
             }
         });
     }
-    $scope.signup = function() {
-        console.log("I'm not done sign-up either")
+    $scope.signout  = function() {
+        tpAuth.logoutUser().then(function() {
+            $scope.username = "";
+            $scope.password = "";
+            tpNotifier.notify('You have successfully signed out');
+            $location.path('/');
+        })
     }
-})
+});
